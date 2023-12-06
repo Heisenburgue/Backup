7d00a11411887c1c5437db037f206159ac6f8708
matheus <Heisenburgue>
2023-12-06 20:12:47 -0300

    primeiro Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function mudaTema() {
+    const body = document.body
+
+    if(body.classList.contains('escuro')) {
+        body.classList.remove('escuro')
+    } else {
+        body.classList.add('escuro')
+    }
+}
